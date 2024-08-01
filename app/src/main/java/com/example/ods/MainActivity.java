1b341db93ae5559f09423fe85f93abae9afccc3d
JHRK <jhrk05@gmail.com>
2024-08-01 11:46:30 -0300

    JSDSDASDA

--- app/src/main/java/com/example/ods/MainActivity.java
+++ app/src/main/java/com/example/ods/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.ods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageButton btnODS11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnODS11 = findViewById(R.id.btnODS11);
+        btnODS11.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent tela11 = new Intent(getApplicationContext(), ODS11.class);
+                startActivity(tela11);
+            }
+        });
     }
 }
