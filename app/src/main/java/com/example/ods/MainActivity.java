950454bb764d40ffd338226393e8ce135e494c04
Luigi de Azevedo Lopes <108826488+LuigiDipp@users.noreply.github.com>
2024-08-01 12:02:12 -0300

    Odêéssi

--- app/src/main/java/com/example/ods/MainActivity.java
+++ app/src/main/java/com/example/ods/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btn_ods7;
+
+        btn_ods7 = findViewById(R.id.btn_ods7);
+        btn_ods7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tela7 = new Intent(getApplicationContext(), ODS7.class);
+                startActivity(tela7);
+            }
+        }
+        );
+
     }
 }
